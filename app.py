1562538e187ae5c5d9d5c6617695db5e5df8ac57
Ashraful <ashrafulalam135@gmail.com>
2026-04-07 11:35:14 +0600

    Sprint 1 environment setup

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="JLPT Study App",
+    layout="wide"
+)
+
+st.title("JLPT Study App")
+
+st.success("Environment setup successful")
---
+++ scripts/init_db.py
@@ -0,0 +1,17 @@
+import sys
+import os
+
+sys.path.append(
+    os.path.dirname(
+        os.path.dirname(
+            os.path.abspath(__file__)
+        )
+    )
+)
+
+from database.db import engine
+from database.models import Base
+
+Base.metadata.create_all(bind=engine)
+
+print("Database created")
